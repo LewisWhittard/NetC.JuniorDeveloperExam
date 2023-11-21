4d2020f4099318de2ebde2605d3af6710e6a5fb8
Lewis <Lewis@DESKTOP-JHNNMRJ>
2023-11-21 18:44:00 +0000

    Add project files.

---
+++ NetC.JuniorDeveloperExam.Web/Contoller/BlogPostController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace NetC.JuniorDeveloperExam.Web.Contoller
+{
+    public class BlogPostController
+    {
+        ActionResult View()
+        {
+
+
+            return View();
+        }
+    }
+}
