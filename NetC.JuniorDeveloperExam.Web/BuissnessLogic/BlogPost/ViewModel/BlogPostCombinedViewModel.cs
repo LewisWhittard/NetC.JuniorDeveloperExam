794be05b720ad269c840eb54cea66686d8b7f8e6
Lewis Whittard <lewis.whittard.dev@googlemail.com>
2023-11-22 16:51:55 +0000

    Comments in foreach loop

--- NetC.JuniorDeveloperExam.Web/BuissnessLogic/BlogPost/ViewModel/BlogPostCombinedViewModel.cs
+++ NetC.JuniorDeveloperExam.Web/BuissnessLogic/BlogPost/ViewModel/BlogPostCombinedViewModel.cs
@@ -8,12 +8,12 @@ namespace NetC.JuniorDeveloperExam.Web.BuissnessLogic.BlogPost.ViewModel
     public class BlogPostCombinedViewModel
     {
         public BlogPostViewModel BlogPost { get; set; }
-        public List<CommentViewModel> Comment { get; set; }
+        public List<CommentViewModel> Comments { get; set; }
 
         public BlogPostCombinedViewModel(BlogPostViewModel bPVM , List<CommentViewModel> cVM) 
         { 
             BlogPost = bPVM;
-            Comment = cVM.ToList();
+            Comments = cVM.ToList();
         }
     }
 }
