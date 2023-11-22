eb56b53011d275c23f98720e227d7a66ce9a51ee
Lewis Whittard <lewis.whittard.dev@googlemail.com>
2023-11-22 21:52:19 +0000

    Add blank page to home controller

--- NetC.JuniorDeveloperExam.Web/Contollers/HomeController.cs
+++ NetC.JuniorDeveloperExam.Web/Contollers/HomeController.cs
@@ -10,7 +10,7 @@ namespace WebApplication1.Controllers
     {
         public ActionResult Index()
         {
-            return RedirectToAction("Index", "BlogPost", new { area = "" });
+            return View();
 
         }
 
